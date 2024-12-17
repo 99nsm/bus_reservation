fb815f56b1da57cbb8e56cfae57ae777abe3c3b7
99nsm <sktkdals1200@gmail.com>
2024-12-17 19:34:21 +0900

    html코드 추가
    회원 가입, 로그인 관련 html 수정및 추가

---
+++ bus_html/js/login.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+// 아이디 찾기 누르면 아이디 팝업 생성
+    $('.id_get').click(function(){
+        window.open('id_popup.html','_blank',"width=600,height=550");
+    });
+
+
+// 비밀번호 찾기 누르면 비밀번호 팝업 생성
+    $('.pw_reset').click(function(){
+        window.open('pw_popup.html','_blank',"width=600,height=550");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
